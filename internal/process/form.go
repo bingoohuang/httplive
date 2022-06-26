b3f7f84202fa4d788828f0515bb51ec1f21cb82a
bingoo <bingoo.huang@gmail.com>
2022-06-26 22:31:01 +0800

    form demo

---
+++ internal/process/form.go
@@ -0,0 +1,30 @@
+package process
+
+import (
+	"github.com/bingoohuang/gg/pkg/emb"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	registerHlHandlers("form", func() HlHandler { return &Form{} })
+}
+
+type Form struct {
+}
+
+func (s Form) HlHandle(c *gin.Context, apiModel *APIDataModel, asset func(name string) string) error {
+	if c.Request.Method == "GET" {
+		emb.ServeFile(subStatic, "form.html", c.Writer, c.Request)
+		return nil
+	}
+
+	c.Request.ParseMultipartForm(32 * 1024 * 1024) // 32M
+	c.Request.ParseForm()
+
+	c.PureJSON(200, map[string]interface{}{
+		"Status": "OK",
+		"Form":   c.Request.Form,
+	})
+
+	return nil
+}
